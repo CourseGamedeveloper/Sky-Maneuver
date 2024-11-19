2880ec5376badf4b042ea91baecdf7a77a0fe8c3
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2024-11-19 22:05:27 +0200

    Update Visibility.cs

--- Assets/Scripts/Visibility.cs
+++ Assets/Scripts/Visibility.cs
@@ -2,26 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class Visibility : MonoBehaviour
 {
     [SerializeField]
-    [Tooltip("The button for orange-airplane to disappear")]
-    public InputAction toggleAction=new InputAction(type:InputActionType.Button);
+    [Tooltip("The button for the orange airplane to disappear.")]
+    public InputAction toggleAction = new InputAction(type: InputActionType.Button);
+
     private Renderer objectRenderer;
+
     void OnEnable()
     {
+        // Enable the toggle action and subscribe to the performed event
         toggleAction.Enable();
         toggleAction.performed += OnToggleVisibility;
     }
 
     void OnDisable()
     {
+        // Unsubscribe from the performed event and disable the toggle action
         toggleAction.performed -= OnToggleVisibility;
         toggleAction.Disable();
     }
 
     void Start()
     {
+        // Get the Renderer component
         objectRenderer = GetComponent<Renderer>();
         if (objectRenderer == null)
         {
@@ -31,6 +37,7 @@ public class Visibility : MonoBehaviour
 
     private void OnToggleVisibility(InputAction.CallbackContext context)
     {
+        // Toggle the visibility of the object
         if (objectRenderer != null)
         {
             objectRenderer.enabled = !objectRenderer.enabled;
