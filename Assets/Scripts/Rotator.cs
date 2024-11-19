3a5d011f9f68face84a7732012f2f7ed7b05f462
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2024-11-19 21:59:37 +0200

    Update Rotator.cs

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -7,7 +7,6 @@ public class Rotator : MonoBehaviour
     [SerializeField]
     [Tooltip("Speed of rotation in degrees per second.")]
     public float rotationSpeed = 100f;
-
     [Tooltip("Axis of rotation.")]
     public Vector3 rotationAxis = Vector3.up;
 
